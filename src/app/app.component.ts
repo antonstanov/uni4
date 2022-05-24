2e63afd419f4ac430efc39a8fb49875e5d8cca5e
AVStanovskiy <avstanovskiy@tsum.ru>
2022-05-25 01:30:20 +0400

    add metaService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'uni4';
+
+  constructor(
+    private metaService: Meta
+  ) {
+    this.metaService.addTag( { name: 'description', content: 'Article Description' })
+  }
 }
